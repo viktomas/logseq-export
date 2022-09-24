1459033064b6a2fdd3a92f39fb22c6ac87574eab
Tomas Vik <tvik@gitlab.com>
2022-09-24 10:45:52 +0200

    testing transforming page

--- transform.go
+++ transform.go
@@ -1,13 +1,23 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func sanitizeName(orig string) string {
 	return strings.ReplaceAll(orig, " ", "-")
 }
 
 func generateFileName(originalName string, attributes map[string]string) string {
-	return sanitizeName(originalName)
+	if _, ok := attributes["slug"]; !ok {
+		return sanitizeName(originalName)
+	}
+	if _, ok := attributes["date"]; ok {
+		return fmt.Sprintf("%s-%s", attributes["date"], attributes["slug"])
+	}
+
+	return attributes["slug"]
 }
 
 func transformPage(p page) page {
--- transform_test.go
+++ transform_test.go
@@ -11,14 +11,43 @@ func TestSanitizeName(t *testing.T) {
 	require.Equal(t, "Blog-idea%3A-All-good-laws-that-EU-brought.md", result)
 }
 
+func GenerateFileName(t *testing.T) {
+
+	t.Run("sanitizes fileName if there's no slug", func(t *testing.T) {
+		result := generateFileName("name with space.md", map[string]string{})
+		require.Equal(t, "name-with-space.md", result)
+	})
+
+	t.Run("combines slug and date into a filename", func(t *testing.T) {
+		result := generateFileName("name with space.md", map[string]string{
+			"slug": "this-is-a-slug",
+			"date": "2022-09-24",
+		})
+		require.Equal(t, "2022-09-24-this-is-a-slug", result)
+	})
+
+	t.Run("combines slug and date into a filename", func(t *testing.T) {
+		result := generateFileName("name with space.md", map[string]string{
+			"slug": "this-is-a-slug",
+			"date": "2022-09-24",
+		})
+		require.Equal(t, "2022-09-24-this-is-a-slug", result)
+	})
+
+}
+
 func TestTransformPage(t *testing.T) {
-	t.Run("sanitizes fileName", func(t *testing.T) {
+
+	t.Run("generates filename", func(t *testing.T) {
 		testPage := page{
-			filename:   "name with space.md",
-			attributes: map[string]string{},
-			text:       "",
+			filename: "name with space.md",
+			attributes: map[string]string{
+				"slug": "this-is-a-slug",
+				"date": "2022-09-24",
+			},
+			text: "",
 		}
 		result := transformPage(testPage)
-		require.Equal(t, "name-with-space.md", result.filename)
+		require.Equal(t, "2022-09-24-this-is-a-slug", result.filename)
 	})
 }
